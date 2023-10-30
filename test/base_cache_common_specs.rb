6de6d58212d75d6d50f030ac0cf523a6680f2688
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-10-30 16:42:19 +0000

    base_cache_common_specs.rb - add rudimentary common tests for save/load

--- test/base_cache_common_specs.rb
+++ test/base_cache_common_specs.rb
@@ -32,4 +32,35 @@ module BaseCacheCommonSpecs
       end
     end
   end
+
+  # This expects subject and cache2 to be cache instances
+  def self.save_load_usecases(path)
+    describe 'loading and saving' do
+      let(:cache) do
+        subject["apple"] = "alpha"
+        subject["banana"] = "beta"
+        subject
+      end
+      
+      it 'should save and load' do
+
+        cache.save(path)
+
+        cache2.load(path)
+
+        # check the size and content are correct
+        value(cache2.hash.size).must_equal(2)
+        value(cache2.hash).must_equal(cache.hash)
+
+        # Add a new entry and save to the default location
+        cache["carrot"] = "gamma"
+        cache.save
+
+        # Should still be equal
+        cache2.load(path)
+        value(cache2.hash.size).must_equal(3)
+        value(cache2.hash).must_equal(cache.hash)
+      end
+    end
+  end
 end
